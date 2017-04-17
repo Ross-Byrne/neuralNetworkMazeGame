3e0f62166d144434efb90d32ecc9a2bf4f0d6399
= <=>
2017-04-17 18:21:40 +0100

    Working on getting spiders moving

--- src/ie/gmit/sw/ai/node/SpiderNode.java
+++ src/ie/gmit/sw/ai/node/SpiderNode.java
@@ -1,5 +1,6 @@
 package ie.gmit.sw.ai.node;
 
+import java.util.*;
 import java.util.concurrent.*;
 
 /**
@@ -8,9 +9,12 @@ import java.util.concurrent.*;
  */
 public class SpiderNode extends Node {
 
+    private long movementSpeed = 3000;
+    private Random rand = new Random();
     private Object lock = null;
     private Node[][] maze = null;
     private ExecutorService executor = Executors.newFixedThreadPool(1);
+    private volatile int moveNum = 0;
 
     public SpiderNode(int row, int col, int id, Object lock, Node[][] maze) {
 
@@ -21,15 +25,77 @@ public class SpiderNode extends Node {
         this.lock = lock;
         this.maze = maze;
 
-        // print out the children
-        System.out.println("Start");
-        System.out.println(adjacentNodes(maze).length);
-        System.out.println("Finish\n");
+        // start moving the spider
+        // run()
+        executor.submit(() -> {
 
-    }
+            while (true) {
+                try {
+
+                    // start moving the spider
+                    move();
+
+                    // sleep thread to simulate a movement pace
+                    Thread.sleep(movementSpeed);
+
+                } catch (Exception ex) {
+
+                } // try catch
+            } // while
+
+        });
+
+    } // constructor
 
 
+    // Method for moving the spider
+    // run in a thread
+    private void move(){
 
+        synchronized (lock) {
+
+            System.out.println("Moving number: " + moveNum);
+
+            Node[] adjacentNodes = null;
+            List<Node> canMoveTo = new ArrayList<>();
+
+            // get the spiders adjacent nodes
+            adjacentNodes = adjacentNodes(maze);
+
+            for (Node n : adjacentNodes) {
+
+                // check that the node is empty space
+                if (n.getId() == -1) {
+
+                    // add node to list of available nodes
+                    canMoveTo.add(n);
+                } // if
+            } // if
+
+            if (canMoveTo.size() > 0) {
+                int newX, newY, oldX, oldY;
+
+                oldX = getRow();
+                oldY = getCol();
+
+                // pick a random index to move to
+                int index = rand.nextInt(canMoveTo.size());
+
+                newX = canMoveTo.get(index).getRow();
+                newY = canMoveTo.get(index).getCol();
+
+                // move to that node
+                maze[newX][newY] = (Node)this;
+
+                // remove self from original spot
+                maze[oldX][oldY] = canMoveTo.get(index);
+
+            } // if
+
+        } // synchronized()
+
+        moveNum++;
+    } // move()
 
 
 
