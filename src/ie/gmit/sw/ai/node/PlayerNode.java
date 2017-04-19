9c7ce96a0e2b76c0517cdcc044bb6cd21136ef9d
Martin Coleman <Martinc94@live.co.uk>
2017-04-19 15:15:08 +0100

    Initialised stats in player node

--- src/ie/gmit/sw/ai/node/PlayerNode.java
+++ src/ie/gmit/sw/ai/node/PlayerNode.java
@@ -6,42 +6,65 @@ package ie.gmit.sw.ai.node;
 public class PlayerNode extends Node {
     private int health;
     private int bombs;
-    private boolean sword;
+    private int swords;
     private int noOfEnemies;
 
     public PlayerNode(int row, int col, int id) {
         super(row, col, id);
+
+        //Init Variables
+        this.health=100;
+        this.bombs=0;
+        this.swords=0;
+        this.noOfEnemies=0;
     }
 
     public int getHealth() {
         return health;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
+    public void increaseHealth(int health) {
+        this.health += health;
+    }
+
+    public void decreaseHealth(int health) {
+        this.health -= health;
     }
 
     public int getBombs() {
         return bombs;
     }
 
-    public void setBombs(int bombs) {
-        this.bombs = bombs;
+    public void increaseBombs(int bombs) {
+        this.bombs += bombs;
     }
 
-    public boolean isSword() {
-        return sword;
+    public void decreaseBombs(int bombs) {
+
+        this.bombs -= bombs;
     }
 
-    public void setSword(boolean sword) {
-        this.sword = sword;
+    public int getSwords() {
+        return swords;
+    }
+
+    public void increaseSwords(int swords) {
+        this.swords += swords;
+    }
+
+    public void decreaseSwords(int swords) {
+        this.swords -= swords;
     }
 
     public int getNoOfEnemies() {
         return noOfEnemies;
     }
 
-    public void setNoOfEnemies(int noOfEnemies) {
-        this.noOfEnemies = noOfEnemies;
+    public void increaseNoOfEnemies(int noOfEnemies) {
+        this.noOfEnemies += noOfEnemies;
+    }
+
+    public void decreaseNoOfEnemies(int noOfEnemies) {
+        this.noOfEnemies -= noOfEnemies;
     }
 }
