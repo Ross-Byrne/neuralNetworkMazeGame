5f17867e219ffb1c31c78cfb0107f8df05050f11
= <=>
2017-04-20 12:46:50 +0100

    Working on combat decisions

--- src/ie/gmit/sw/ai/node/PlayerNode.java
+++ src/ie/gmit/sw/ai/node/PlayerNode.java
@@ -36,15 +36,52 @@ public class PlayerNode extends Node {
         // decide what to do
         try {
 
+            /*
+                1 = Health (1 = Healthy, 0.5 = Minor Injuries, 0 = Serious Injuries)
+                2 = Has Sword (1 = Yes, 0 = No)
+                3 = Has Bomb (1 = Yes, 0 = No)
+                4 = Enemies Status (0 = One, 0.5 = Two, 1 = Three or More)
+             */
+
+            double healthStatus = 1;
+            double swordStatus = 0;
+            double bombStatus = 0;
+            double enemyStatus = 0;
+
             // set health in health classifier
             fuzzyHealthClassifier.setInputVariable("health", getHealth());
 
             // get the health stat from fuzzy health classifier
             String injuriesStatus = fuzzyHealthClassifier.getWinningMembership("injuries");
 
-            System.out.println(injuriesStatus);
+            // get health status
+            switch (injuriesStatus){
+                case "none":
+                    healthStatus = 1;
+                    break;
+                case "minor":
+                    healthStatus = 0.5;
+                    break;
+                case "serious":
+                    healthStatus = 0;
+                    break;
+            } // switch
 
-            combatNet.action(1, 1, 0, 0);
+            System.out.println(injuriesStatus + " = " + healthStatus);
+
+            // get enemy status
+
+            // get sword status
+            if(isSword())
+                swordStatus = 1;
+
+
+            // get bomb status
+            if(getBombs() > 0)
+                bombStatus = 1;
+
+            // get combat decision
+            combatNet.action(healthStatus, swordStatus, bombStatus, 0);
 
         }catch (Exception ex){
 
